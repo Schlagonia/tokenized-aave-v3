7d692b0ce72bcd2edb3e693fa82d842daad0eb29
Schlagonia <schlagonia@gmail.com>
2023-11-21 13:35:12 -0700

    build: apr oracle

--- scripts/Deploy.py
+++ scripts/Deploy.py
@@ -3,13 +3,13 @@ from ape import accounts, project, networks
 
 
 def deploy():
-    signer = accounts.load("v3_deployer")
+    signer = accounts.load("")
 
     signer.deploy(
         project.AaveV3LenderFactory,
-        "0xB865AAf1f9f60630934739595f183C4900f65ed9",
-        "0xB865AAf1f9f60630934739595f183C4900f65ed9",
-        "0xd9e53c8b326fddcdbcf225d1f7be487e1f01bd0b",
+        "",
+        "",
+        "",
         publish=True,
     )
 
@@ -21,5 +21,5 @@ def publish():
 
 
 def main():
-    # publish()
+    #publish()
     deploy()
