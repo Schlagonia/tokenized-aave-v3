368ae74c21cf7d3a758a9588ca29fcc40ae071b8
Schlagonia <schlagonia@gmail.com>
2024-10-08 09:50:37 -0600

    data provider

--- src/periphery/StrategyAprOracle.sol
+++ src/periphery/StrategyAprOracle.sol
@@ -12,7 +12,7 @@ contract StrategyAprOracle {
         IPool(0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2);
 
     IProtocolDataProvider public constant protocolDataProvider =
-        IProtocolDataProvider(0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3);
+        IProtocolDataProvider(0x41393e5e337606dc3821075Af65AeE84D7688CBD);
 
     /**
      * @notice Will return the expected Apr of a strategy post a debt change.
