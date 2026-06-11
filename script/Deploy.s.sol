a517d05904cea4cf8963fe3b92b3aa16e05dec46
Schlagonia <schlagonia@gmail.com>
2026-06-10 21:29:10 -0600

    chore: bump ts version

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -27,7 +27,7 @@ contract Deploy is Script {
             0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
         );
 
-        console.log("Oracle deployed at", address(oracle));
+        //console.log("Oracle deployed at", address(oracle));
 
         address usdcLender = factory.newSparkLender(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
         console.log("USDC Lender deployed at", usdcLender);
